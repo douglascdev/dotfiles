712a592af7307ab4abca79c702493b6b01af5671
douglas <douglasc.dev@gmail.com>
2025-10-19 12:01:21 -0300

    steam and bottles

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flatpak install sh.loft.devpod org.mozilla.firefox com.chatterino.chatterino com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian io.podman_desktop.PodmanDesktop com.ranfdev.DistroShelf com.github.tchx84.Flatseal
+flatpak install sh.loft.devpod org.mozilla.firefox com.chatterino.chatterino com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian io.podman_desktop.PodmanDesktop com.ranfdev.DistroShelf com.github.tchx84.Flatseal com.usebottles.bottles com.valvesoftware.Steam
