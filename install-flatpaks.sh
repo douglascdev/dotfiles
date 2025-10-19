6970580492495fbc7ada19840c08bc5d9b6ca622
Douglas <douglasc.dev@gmail.com>
2025-10-18 23:38:58 -0300

    install flatpaks

---
+++ install-flatpaks.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flatpak install sh.loft.devpod org.mozilla.firefox com.chatterino.chatterino com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian io.podman_desktop.PodmanDesktop com.ranfdev.DistroShelf com.github.tchx84.Flatseal
