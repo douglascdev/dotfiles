ec4d26a6ad044107dd17033e9dc93d4f6c2c8405
douglas <douglasc.dev@gmail.com>
2025-10-21 16:30:07 -0300

    remove

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# set up dot files in devpod: https://devpod.sh/docs/developing-in-workspaces/dotfiles-in-a-workspace
-
-cd /home/$USER
-mv dotfiles/* .
-mv dotfiles/.* .
-
-git submodule init
-git submodule update
